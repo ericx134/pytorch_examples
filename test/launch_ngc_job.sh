e608f64454883dd33fbbc669a47139b9158c51c4
ericx134 <ericx@stanford.edu>
2018-09-26 10:25:59 -0700

    dlss-ngc

--- test/launch_ngc_job.sh
+++ test/launch_ngc_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CMD='cd /workspace/code/dlaa-pytorch-master; python3 -m apex.parallel.multiproc dlaa.py '
-CMD+='--label=MyNewTraining --paths="[/workspace/data/HumanRef_480x540]" '
+CMD+='--label=MyNewTraining --paths="[/dataset]" '
 CMD+='--epochs=100          --log_interval=10       --save_interval=10  --disable_vgg '
 CMD+='--batch_size=32       --train                 --tensorboard       --valperc=0 '
-CMD+='--io_workers=10       --learning_rate=0.0001  --clr_min=0.0001    --clr_max=0.001 '
+CMD+='--io_workers=10        --learning_rate=0.0001  --clr_min=0.0001    --clr_max=0.001 '
 CMD+='--multigpu_mode="DDP" --tfx="BasicPQEncodingFunction"'
 
 ngc batch run \
@@ -14,5 +14,6 @@ ngc batch run \
   --name "Testing DLSS training on NGC" \
   --image "nvidian/dt/dlaa-pytorch:pth040_cuda9010_py36_apex_gputil" \
   --workspace ericx-dlaa:/workspace \
+  --datasetid 12525:/dataset \
   --result /result \
   --commandline "$CMD"
