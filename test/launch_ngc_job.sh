d596203027e264482f2d0ce3ce85ef51835025fb
ericx <eric@stanford.edu>
2018-09-26 17:18:32 -0700

    mvcheck.zip

--- test/launch_ngc_job.sh
+++ test/launch_ngc_job.sh
@@ -4,7 +4,7 @@ CMD='cd /workspace/code/dlaa-pytorch-master; python3 -m apex.parallel.multiproc 
 CMD+='--label=MyNewTraining --paths="[/dataset]" '
 CMD+='--epochs=100          --log_interval=10       --save_interval=10  --disable_vgg '
 CMD+='--batch_size=32       --train                 --tensorboard       --valperc=0 '
-CMD+='--io_workers=10        --learning_rate=0.0001  --clr_min=0.0001    --clr_max=0.001 '
+CMD+='--io_workers=4        --learning_rate=0.0001  --clr_min=0.0001    --clr_max=0.001 '
 CMD+='--multigpu_mode="DDP" --tfx="BasicPQEncodingFunction"'
 
 ngc batch run \
